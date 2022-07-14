c396fb75f137d68594b84846671c56173499661d
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-07-14 16:03:13 +0200

    Just a boards view

---
+++ src/pages/boards/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const boards = ref([
+  {
+    id: 1,
+    name: 'Board 1'
+  },
+  {
+    id: 2,
+    name: 'Board 2'
+  },
+  {
+    id: 3,
+    name: 'Board 3'
+  }
+])
+</script>
+<template>
+  <div class="container">
+    <div>Boards View</div>
+    <div>{{ boards }}</div>
+  </div>
+</template>
